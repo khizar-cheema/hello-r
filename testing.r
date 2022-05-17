51af280fa3874183305c351452643d0e68718a0e
khizar hayat <khizar.cheemakh@gmail.com>
2022-05-17 17:27:13 +0300

    some lines has been added

--- testing.r
+++ testing.r
@@ -1,3 +1,7 @@
 # This is from Github
 print ("hello from Github")
-
+name="khizar hayat"
+version="git hub in R-studio"
+print(version)
+print("these lines has been added in R")
+print("ahmed is saying he is fine")
